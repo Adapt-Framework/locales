7e9043556914c6b46c9992d7972c5c12c7bf4f0f
Matt Bruton <matt.bruton@gmail.com>
2015-05-29 15:29:13 +0100

    Added new fields and made this bundle dependent on forms.... then changed my mind and undid everything!

--- boot.php
+++ boot.php
@@ -5,7 +5,7 @@ defined('ADAPT_STARTED') or die;
 
 $adapt = $GLOBALS['adapt'];
 
-$adapt->sanitizer->add_unformatter('phone', function($value){
+$adapt->sanitize->add_unformatter('phone', function($value){
     return preg_replace("/[\d+]/", "", $value);
 });
 
