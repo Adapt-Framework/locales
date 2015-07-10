39d649a639ab9106024be4a785687f254c1f9636
Matt Bruton <matt.bruton@gmail.com>
2015-07-10 15:32:12 +0100

    Various updates

--- boot.php
+++ boot.php
@@ -5,8 +5,11 @@ defined('ADAPT_STARTED') or die;
 
 $adapt = $GLOBALS['adapt'];
 
-$adapt->sanitize->add_unformatter('phone', function($value){
+$adapt->sanitize->add_unformat('phone', function($value){
     return preg_replace("/[\d+]/", "", $value);
-});
+}, "function(value){
+    value = value.replace(/[^0-9]/g, '');
+    return value;
+}");
 
 ?>
