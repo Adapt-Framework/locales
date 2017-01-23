e464df37876b3b5fdd74bb4ae711d2ca44392efb
Matt Bruton <mb@deliowealth.com>
2017-01-23 18:11:06 +0000

    Updates

--- classes/bundle_locales.php
+++ classes/bundle_locales.php
@@ -14,171 +14,218 @@ namespace adapt\locales{
         public function boot(){
             if (parent::boot()){
 
-                    $adapt = $this;
-                    
-                    $adapt->sanitize->add_format(
-                        'locales_date',
-                        function($value){
-                            /*
-                             * INPUT: Y-m-d
-                             */
-                            $adapt = $GLOBALS['adapt'];
-                            if ($value && !is_null($adapt->setting('locales.default_date_format'))){
-                                return $adapt->sanitize->format($adapt->setting('locales.default_date_format'), $value);
-                            }
-                            
-                            return $value;
-                        }, "function(value){
-                            if (value && adapt.setting('locales.default_date_format') != null){
-                                return adapt.sanitize.format(adapt.setting('locales.default_date_format'), value);
-                            }
-                            
-                            return value;
-                        }"
-                    );
-                    
-                    $adapt->sanitize->add_format(
-                        'locales_time',
-                        function($value){
-                            /*
-                             * INPUT: H:i:s
-                             */
-                            $adapt = $GLOBALS['adapt'];
-                            if ($value && !is_null($adapt->setting('locales.default_time_format'))){
-                                return $adapt->sanitize->format($adapt->setting('locales.default_time_format'), $value);
-                            }
-                            
-                            return $value;
-                        }, "function(value){
-                            if (value && adapt.setting('locales.default_time_format') != null){
-                                return adapt.sanitize.format(adapt.setting('locales.default_time_format'), value);
-                            }
-                            
-                            return value;
-                        }"
-                    );
-                    
-                    $adapt->sanitize->add_format(
-                        'locales_datetime',
-                        function($value){
-                            /*
-                             * INPUT: Y-m-d H:i:s
-                             */
-                            $adapt = $GLOBALS['adapt'];
-                            if ($value){
-                                if (!is_null($adapt->setting('locales.default_datetime_format'))){
-                                    return $adapt->sanitize->format($adapt->setting('locales.default_datetime_format'), $value);
-                                }
-                            }      
-                            return $value;
-                        }, "function(value){
-                            if (value && adapt.setting('locales.default_datetime_format') != null){
-                                return adapt.sanitize.format(adapt.setting('locales.default_datetime_format'), value);
-                            }
-                            
-                            return value;
-                        }"
-                    );
-                    
-                    
-                    
-                    $adapt->sanitize->add_unformat(
-                        'locales_date',
-                        function($value){
-                            /*
-                             * INPUT: Y-m-d
-                             */
-                            $adapt = $GLOBALS['adapt'];
-                            if (!is_null($adapt->setting('locales.default_date_format'))){
-                                return $adapt->sanitize->unformat($adapt->setting('locales.default_date_format'), $value);
-                            }
-                            
-                            return $value;
-                        }, "function(value){
-                            if (adapt.setting('locales.default_date_format') != null){
-                                return adapt.sanitize.unformat(adapt.setting('locales.default_date_format'), value);
-                            }
-                            
-                            return value;
-                        }"
-                    );
-                    
-                    $adapt->sanitize->add_unformat(
-                        'locales_time',
-                        function($value){
-                            /*
-                             * INPUT: H:i:s
-                             */
-                            $adapt = $GLOBALS['adapt'];
-                            if (!is_null($adapt->setting('locales.default_time_format'))){
-                                return $adapt->sanitize->unformat($adapt->setting('locales.default_time_format'), $value);
-                            }
-                            
-                            return $value;
-                        }, "function(value){
-                            if (adapt.setting('locales.default_time_format') != null){
-                                return adapt.sanitize.unformat(adapt.setting('locales.default_time_format'), value);
-                            }
-                            
-                            return value;
-                        }"
-                    );
-                    
-                    $adapt->sanitize->add_unformat(
-                        'locales_datetime',
-                        function($value){
-                            /*
-                             * INPUT: Y-m-d H:i:s
-                             */
-                            $adapt = $GLOBALS['adapt'];
-                            if (!is_null($adapt->setting('locales.default_datetime_format'))){
-                                return $adapt->sanitize->unformat($adapt->setting('locales.default_datetime_format'), $value);
-                            }
-                            
-                            return $value;
-                        }, "function(value){
-                            if (adapt.setting('locales.default_datetime_format') != null){
-                                return adapt.sanitize.unformat(adapt.setting('locales.default_datetime_format'), value);
-                            }
-                            
-                            return value;
-                        }"
-                    );
-                    
-                    
-                    
-                    $adapt->sanitize->add_unformat('phone', function($value){
-                        return preg_replace("/[\d+]/", "", $value);
+                $adapt = $this;
+
+                $adapt->sanitize->add_format(
+                    'locales_date',
+                    function($value){
+                        /*
+                         * INPUT: Y-m-d
+                         */
+                        $adapt = $GLOBALS['adapt'];
+                        if ($value && !is_null($adapt->setting('locales.default_date_format'))){
+                            return $adapt->sanitize->format($adapt->setting('locales.default_date_format'), $value);
+                        }
+
+                        return $value;
                     }, "function(value){
-                        value = value.replace(/[^0-9]/g, '');
+                        if (value && adapt.setting('locales.default_date_format') != null){
+                            return adapt.sanitize.format(adapt.setting('locales.default_date_format'), value);
+                        }
+
                         return value;
-                    }");
-                    
-                    
-                    /*
-                     * Add an event listener and listen
-                     * for the adapt ready event so that we
-                     * can add settings to the page
-                     */
-                    $global_adapt = $GLOBALS['adapt'];
-                    $global_adapt->on(
-                        \adapt\base::EVENT_READY,
-                        function($event){
-                            $adapt = $GLOBALS['adapt'];
-                            if ($adapt->dom && $adapt->dom instanceof \adapt\page){
-                                
-                                $datetime = $adapt->data_source->get_data_type($adapt->setting('locales.default_datetime_format'));
-                                $date = $adapt->data_source->get_data_type($adapt->setting('locales.default_date_format'));
-                                $time = $adapt->data_source->get_data_type($adapt->setting('locales.default_time_format'));
-                                $adapt->dom->head->add(new html_meta(array('class' => 'setting', 'name' => 'locales.default_date_format', 'content' => $date['name'])));
-                                $adapt->dom->head->add(new html_meta(array('class' => 'setting', 'name' => 'locales.default_time_format', 'content' => $time['name'])));
-                                $adapt->dom->head->add(new html_meta(array('class' => 'setting', 'name' => 'locales.default_datetime_format', 'content' => $datetime['name'])));
-                                $adapt->dom->head->add(new html_meta(array('class' => 'setting', 'name' => 'locales.default_date_pattern', 'content' => $date['datetime_format'])));
-                                $adapt->dom->head->add(new html_meta(array('class' => 'setting', 'name' => 'locales.default_time_pattern', 'content' => $time['datetime_format'])));
-                                $adapt->dom->head->add(new html_meta(array('class' => 'setting', 'name' => 'locales.default_datetime_pattern', 'content' => $datetime['datetime_format'])));
+                    }"
+                );
+
+                $adapt->sanitize->add_format(
+                    'locales_time',
+                    function($value){
+                        /*
+                         * INPUT: H:i:s
+                         */
+                        $adapt = $GLOBALS['adapt'];
+                        if ($value && !is_null($adapt->setting('locales.default_time_format'))){
+                            return $adapt->sanitize->format($adapt->setting('locales.default_time_format'), $value);
+                        }
+
+                        return $value;
+                    }, "function(value){
+                        if (value && adapt.setting('locales.default_time_format') != null){
+                            return adapt.sanitize.format(adapt.setting('locales.default_time_format'), value);
+                        }
+
+                        return value;
+                    }"
+                );
+
+                $adapt->sanitize->add_format(
+                    'locales_datetime',
+                    function($value){
+                        /*
+                         * INPUT: Y-m-d H:i:s
+                         */
+                        $adapt = $GLOBALS['adapt'];
+                        if ($value){
+                            if (!is_null($adapt->setting('locales.default_datetime_format'))){
+                                return $adapt->sanitize->format($adapt->setting('locales.default_datetime_format'), $value);
                             }
+                        }      
+                        return $value;
+                    }, "function(value){
+                        if (value && adapt.setting('locales.default_datetime_format') != null){
+                            return adapt.sanitize.format(adapt.setting('locales.default_datetime_format'), value);
+                        }
+
+                        return value;
+                    }"
+                );
+
+
+
+                $adapt->sanitize->add_unformat(
+                    'locales_date',
+                    function($value){
+                        /*
+                         * INPUT: Y-m-d
+                         */
+                        $adapt = $GLOBALS['adapt'];
+                        if (!is_null($adapt->setting('locales.default_date_format'))){
+                            return $adapt->sanitize->unformat($adapt->setting('locales.default_date_format'), $value);
+                        }
+
+                        return $value;
+                    }, "function(value){
+                        if (adapt.setting('locales.default_date_format') != null){
+                            return adapt.sanitize.unformat(adapt.setting('locales.default_date_format'), value);
+                        }
+
+                        return value;
+                    }"
+                );
+
+                $adapt->sanitize->add_unformat(
+                    'locales_time',
+                    function($value){
+                        /*
+                         * INPUT: H:i:s
+                         */
+                        $adapt = $GLOBALS['adapt'];
+                        if (!is_null($adapt->setting('locales.default_time_format'))){
+                            return $adapt->sanitize->unformat($adapt->setting('locales.default_time_format'), $value);
+                        }
+
+                        return $value;
+                    }, "function(value){
+                        if (adapt.setting('locales.default_time_format') != null){
+                            return adapt.sanitize.unformat(adapt.setting('locales.default_time_format'), value);
+                        }
+
+                        return value;
+                    }"
+                );
+
+                $adapt->sanitize->add_unformat(
+                    'locales_datetime',
+                    function($value){
+                        /*
+                         * INPUT: Y-m-d H:i:s
+                         */
+                        $adapt = $GLOBALS['adapt'];
+                        if (!is_null($adapt->setting('locales.default_datetime_format'))){
+                            return $adapt->sanitize->unformat($adapt->setting('locales.default_datetime_format'), $value);
+                        }
+
+                        return $value;
+                    }, "function(value){
+                        if (adapt.setting('locales.default_datetime_format') != null){
+                            return adapt.sanitize.unformat(adapt.setting('locales.default_datetime_format'), value);
+                        }
+
+                        return value;
+                    }"
+                );
+
+
+
+                $adapt->sanitize->add_unformat('phone', function($value){
+                    return preg_replace("/[\d+]/", "", $value);
+                }, "function(value){
+                    value = value.replace(/[^0-9]/g, '');
+                    return value;
+                }");
+                
+                /**
+                 * Add country to base so that locales information is easily accessible
+                 */
+                \adapt\base::extend(
+                    'pget_country',
+                    function($_this){
+                        $country = $_this->store('locales.country');
+                        if (!$country instanceof \adapt\model && $country->table_name != 'country'){
+                            $country = new model_country();
+                        }
+                        
+                        if ($country->name != $_this->setting('locales.default_country')){
+                            $country->load_by_name($_this->setting('locales.default_country'));
+                        }
+                        
+                        $_this->store('locales.country', $country);
+                        
+                        return $country;
+                    }
+                );
+                
+                /**
+                 * Add an event listener to the DOM and
+                 * append locales information on render
+                 */
+                if ($this->dom instanceof \adapt\page && $this->dom->tag == 'html'){
+                    $this->dom->listen(
+                        \adapt\page::EVENT_RENDER, 
+                        function($event){
+                            if ($event['object']->country->is_loaded){
+                                print_r($event['object']->country->to_hash());
+                            }else{
+                                // Set defaults
+                                $event['object']->head->add(new html_meta(['class' => 'setting', 'name' => 'locales.default_date_format', 'content' => 'date']));
+                                $event['object']->head->add(new html_meta(['class' => 'setting', 'name' => 'locales.default_time_format', 'content' => 'time']));
+                                $event['object']->head->add(new html_meta(['class' => 'setting', 'name' => 'locales.default_datetime_format', 'content' => 'datetime']));
+                                $event['object']->head->add(new html_meta(['class' => 'setting', 'name' => 'locales.default_date_pattern', 'content' => 'Y-m-d']));
+                                $event['object']->head->add(new html_meta(['class' => 'setting', 'name' => 'locales.default_time_pattern', 'content' => 'H:i:s']));
+                                $event['object']->head->add(new html_meta(['class' => 'setting', 'name' => 'locales.default_datetime_pattern', 'content' => 'Y-m-d H:i:s']));
+                            }
+                            print_r($event);
+                            exit(1);
                         }
                     );
+                }
+
+                /*
+                 * Add an event listener and listen
+                 * for the adapt ready event so that we
+                 * can add settings to the page
+                 */
+                $global_adapt = $GLOBALS['adapt'];
+                $global_adapt->on(
+                    \adapt\base::EVENT_READY,
+                    function($event){
+                        $adapt = $GLOBALS['adapt'];
+                        if ($adapt->dom && $adapt->dom instanceof \adapt\page){
+
+                            $datetime = $adapt->data_source->get_data_type($adapt->setting('locales.default_datetime_format'));
+                            $date = $adapt->data_source->get_data_type($adapt->setting('locales.default_date_format'));
+                            $time = $adapt->data_source->get_data_type($adapt->setting('locales.default_time_format'));
+                            $adapt->dom->head->add(new html_meta(array('class' => 'setting', 'name' => 'locales.default_date_format', 'content' => $date['name'])));
+                            $adapt->dom->head->add(new html_meta(array('class' => 'setting', 'name' => 'locales.default_time_format', 'content' => $time['name'])));
+                            $adapt->dom->head->add(new html_meta(array('class' => 'setting', 'name' => 'locales.default_datetime_format', 'content' => $datetime['name'])));
+                            $adapt->dom->head->add(new html_meta(array('class' => 'setting', 'name' => 'locales.default_date_pattern', 'content' => $date['datetime_format'])));
+                            $adapt->dom->head->add(new html_meta(array('class' => 'setting', 'name' => 'locales.default_time_pattern', 'content' => $time['datetime_format'])));
+                            $adapt->dom->head->add(new html_meta(array('class' => 'setting', 'name' => 'locales.default_datetime_pattern', 'content' => $datetime['datetime_format'])));
+                        }
+                    }
+                );
+                
+                    
                 
                 
                 return true;
---
+++ models/model_country.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace \adapt\locales;
+
+class model_country extends \adapt\model {
+    
+    protected $_date_data_type;
+    protected $_time_data_type;
+    protected $_datetime_data_type;
+    
+    public function __construct($id = null, $data_source = null){
+        parent::__construct("country", $id, $data_source);
+    }
+    
+    public function pget_date_data_type(){
+        if (!$this->_date_data_type){
+            $this->_date_data_type = new model_data_type();
+        }
+        
+        if (!$this->_date_data_type->is_loaded || $this->_date_data_type->data_type_id != $this->date_data_type_id){
+            $this->_date_data_type->load($this->date_data_type_id);
+        }
+        
+        return $this->_date_data_type;
+    }
+    
+    public function pget_time_data_type(){
+        if (!$this->_time_data_type){
+            $this->_time_data_type = new model_data_type();
+        }
+        
+        if (!$this->_time_data_type->is_loaded || $this->_time_data_type->data_type_id != $this->time_data_type_id){
+            $this->_time_data_type->load($this->time_data_type_id);
+        }
+        
+        return $this->_time_data_type;
+    }
+    
+    public function pget_datetime_data_type(){
+        if (!$this->_datetime_data_type){
+            $this->_datetime_data_type = new model_data_type();
+        }
+        
+        if (!$this->_datetime_data_type->is_loaded || $this->_datetime_data_type->data_type_id != $this->datetime_data_type_id){
+            $this->_datetime_data_type->load($this->datetime_data_type_id);
+        }
+        
+        return $this->_datetime_data_type;
+    }
+}
